920975d5090baae33c0e7a8a06b7a266d77e14a7
Todd Hay <todd.hay@noaa.gov>
2019-06-11 18:41:31 -0700

    dockerfile updates

---
+++ jenkins/deploy/Makefile.properties.mk
@@ -0,0 +1,15 @@
+# makefile defining workstation/developer-specific variables
+#
+#
+# Instructions:
+# Make your local changes to this file, then run:
+#   git update-index --assume-unchanged Makefile.properties.mk
+# (if you later want to modify the version controlled, base template you can
+# revert, resume tracking, & make your edits to commit, via:
+#   git checkout -- Makefile.properties.mk
+#   git update-index --no-assume-unchanged Makefile.properties.mk
+#   (edit the template file)
+#   git commit Makefile.properties.mk
+#   git update-index --assume-unchanged Makefile.properties.mk
+
+docker_image_suffix = saildrone-data-pull
